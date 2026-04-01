91106f29c3551dfee9468366c9005a9cd5bf9252
danah04 <danahaldosary1@gmail.com>
2026-04-01 11:32:29 +0300

    completed todo 3

--- calculator.js
+++ calculator.js
@@ -219,3 +219,25 @@ import _ from "lodash";
 
 const operation = process.argv[2];
 const numbers = process.argv.slice(3);
+
+if (!isValidOperation(operation)) {
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide");
+  process.exit(1);
+}
+
+const nums = parseNumbers(numbers);
+let result;
+
+switch (operation) {
+  case "add":
+    result = add(nums);
+    break;
+  case "subtract":
+    result = subtract(nums);
+    break;
+  default:
+    console.log("Operation not implemented yet.");
+    process.exit(1);
+}
+
+console.log(`Result: ${result}`); 
