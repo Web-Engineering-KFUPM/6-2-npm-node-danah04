0d0603135a7b92b1dfc30ad0c6ffd5b70afc1b2b
danah04 <danahaldosary1@gmail.com>
2026-04-01 11:37:11 +0300

    completed todo 4

--- calculator.js
+++ calculator.js
@@ -240,4 +240,19 @@ switch (operation) {
     process.exit(1);
 }
 
-console.log(`Result: ${result}`); 
+console.log(`Result: ${result}`);
+
+export function add(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+export function subtract(numbers) {
+  return numbers.reduce((diff, num) => diff - num);
+}
+
+export function multiply(numbers) {
+  return numbers.reduce((prod, num) => prod * num, 1);
+}
+
+export function divide(numbers) {
+  return numbers.reduce((quot, num) => quot / num);
+}
